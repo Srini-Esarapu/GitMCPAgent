57ef3c0d46f3f6a2ae7db463ce443efa9c4bf5ee
Srini-Esarapu <srinivas.esarapu@bayer.com>
2025-12-27 00:06:56 +0530

    Update calc2.py

--- calc2.py
+++ calc2.py
@@ -1,10 +1,1 @@
-def add(a, b):
-    return a + b  
-def subtract(a, b):
-    return a - b
-def multiply(a, b):
-    return a * b  
-def divide(a, b):
-    if b == 0:
-        raise ValueError("Cannot divide by zero.")  
-add(1,2)
+# Test your App
